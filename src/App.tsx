9e4e3c1bd2fecb0e1b9dffec4f561375c643bb66
kimmolepola <kimmo.lepola@gmail.com>
2022-07-24 18:43:10 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ImHome } from 'react-icons/im';
+
+const App = () => (
+  <div className="flex shrink text-2xl">
+    <div className="p-4 text-rose-600 bg-gradient-to-r from-blue-100 to-emerald-300">
+      <ImHome />
+      &#9776;
+      hello
+    </div>
+  </div>
+);
+
+export default App;
+
+// hello
